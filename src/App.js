80e87573ecaa143d10c9cdf3353d5404c27a420e
Susan Wairimu <mwihaki2susan@gmail.com>
2021-10-21 17:52:50 +0300

    hangman game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import { showNotification as Show} from './helpers/Helper';
+import PopUp from './components/PopUp';
+import Notification from './components/Notification';
+
+
+
+const words = ['application', 'programming', 'interface','wizard','developer'];
+let selectedWord = words[Math.floor(Math.random()* words.length)];
+
+let playable = true;
+
+const correctLetters =[];
+const wrongLetters = [];
 
 function App() {
+const [playable, setPlayable] = useState(true);
+const [correctLetters, setCorrectLetters] = useState([]);
+const [wrongLetters, setWrongLetters] = useState([]);
+const [showNotification, setShowNotification]= useState(false);
+
+
+
+useEffect(() => {
+const handleKeydown = e =>{
+  const [key, keyCode] = e;
+
+  if(playable){
+    if(playable && keyCode >= 65 && keyCode <=90){
+      const letter = key.toLowerCase();
+
+      if(selectedWord.includes(letter)){
+        if(! correctLetters.includes(letter)){
+          setCorrectLetters(currentLetters =>[...currentLetters,letter]);
+
+        }else{
+          Show(setShowNotification);
+        }
+      }else{
+        if(! wrongLetters.includes(letter)){
+          setWrongLetters(wrongLetters =>[...wrongLetters,letter]);
+        }else{
+          Show(setShowNotification);
+        }
+      }
+    }
+  }
+  window.addEventListener('keydown', handleKeydown);
+  return()=>window.removeEventListener('keydown', handleKeydown);
+
+}
+
+},[correctLetters,wrongLetters,playable])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<Header />
+<div className='game-container'>
+<Figure wrongLetters={wrongLetters}/>
+<WrongLetters wrongLetters={wrongLetters}/>
+<Word  selectedWord={selectedWord} correctLetters={correctLetters}/>
+</div>
+<PopUp/>
+<Notification showNotification={showNotification}/>
+    </>
   );
 }
 
---
+++ src/components/PopUp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PopUp = () => {
+    return (
+        <div className='popup-container' id='popup-container'>
+<div className='popup'>
+    <h2 id='Final-message'> </h2>
+    <h3 id='final message reveal word'> </h3>
+    <button id='play-button'>Play again</button>
+    </div> 
+        </div>
+    );
+}
+
+export default PopUp;
